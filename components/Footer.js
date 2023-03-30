23e0c14327cd73f41e5f94f71d563d327bbe9b11
Anubhavgarg1910 <utkarshgarg422@example.com>
2023-03-31 01:29:23 +0530

    build day one

---
+++ components/Footer.js
@@ -0,0 +1,54 @@
+
+
+const Footer = () => {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600 ">
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>Its a preety awesome clone</p>
+                <p>Referrals Accepted</p>
+                <p>Gargfam</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>Anubhav Garg</p>
+                <p>Presents</p>
+                <p>Airbnb Clone</p>
+                <p>Lots of time</p>
+                <p>Efforts</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help Center</p>
+                <p>Trust & Safety</p>
+                <p>Say Hi Garg</p>
+                <p>Cheesey Pizza</p>
+                <p>For the Win</p>
+            </div>
+
+
+
+
+
+
+
+
+
+
+        </div>
+    )
+}
+
+
+export default Footer
